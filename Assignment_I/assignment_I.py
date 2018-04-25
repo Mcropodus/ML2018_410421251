3ddf71c03835639167594c5435a9badd756d9cbe
Macropodus <orangeandy12@gmail.com>
2018-04-25 22:24:34 +0800

    AssignmentI_I

---
+++ Assignment_I/assignment_I.py
@@ -0,0 +1,24 @@
+import numpy as np
+from PIL import Image
+
+img1 = "E.png"
+img2 = "I.png"
+img3 = "key1.png"
+img4 = "key2.png"
+
+e = Image.open(img1)
+i = Image.open(img2)
+k1 = Image.open(img3)
+k2 = Image.open(img4)
+
+# e = w1k1 + w2k2 + w3i
+
+epoch = 1
+
+
+
+
+
+
+
+
