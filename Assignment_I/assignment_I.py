d4f49cea6f95b284cdaef5d61a012cfbe4118dad
Macropodus <orangeandy12@gmail.com>
2018-04-26 14:17:44 +0800

    Assignment I_II

--- Assignment_I/assignment_I.py
+++ Assignment_I/assignment_I.py
@@ -10,15 +10,21 @@ e = Image.open(img1)
 i = Image.open(img2)
 k1 = Image.open(img3)
 k2 = Image.open(img4)
+W = 400
+H = 300
+temp = np.zeros((W, H), int) # an initial W*H list
+E = np.array(e) # transform img to array
+# print E
+I = np.array(i)
+K1 = np.array(k1)
+K2 = np.array(k2)
+w = [0, 0, 0]
+# print temp
+
+def Learing(W, H, E, I, K1, K2, w, temp): # define a function to learn the value of w
+    for y in range(H):
+        for x in range(W):
+
 
 # e = w1k1 + w2k2 + w3i
-
-epoch = 1
-
-
-
-
-
-
-
-
+Learing(W, H, E, I, K1, K2, w, temp)
