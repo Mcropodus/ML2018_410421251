765dfe73b270d918e4bc22f300a9ecc8d9ac49f9
Macropodus <orangeandy12@gmail.com>
2018-04-30 23:57:44 +0800

    Assignment I_VII

--- Assignment_I/assignment_I.py
+++ Assignment_I/assignment_I.py
@@ -15,30 +15,30 @@ H = 300
 #temp = np.zeros((W, H), int) # an initial W*H list
 E = np.array(e) # transform img to array
 # print E
-I = np.array(i)
+I1 = np.array(i)
 K1 = np.array(k1)
 K2 = np.array(k2)
-w1 = [0, 0, 0]
+w = [0, 0, 0]
 # print temp
 
-def Learing(W, H, E, I, K1, K2, w): # define a function to learn the value of w
-    m = [K1, K2, I]
+def Learing(W, H, E, I1, K1, K2, w): # define a function to learn the value of w
+    #m = [K1, K2, I1]
     a = []
     e = []
-    apha = 0.000001
+    apha = 0.00001
     epoch = 1
     y = 0
     #while (epoch==1 or epoch<(W*H) and np.abs(w[y]-w[y-1])>0):
     for x in range(W*H):
-        a[x] = w[0]*K1[x] + w[1]*K2 + w[3]*K3
+        a[x] = w[0]*K1[x] + w[1]*K2[x] + w[3]*K3[x]
         e[x] = E[x] - a[x]
         w[0] = w[0] + apha*e[x]*K1[x]
         w[1] = w[1] + apha*e[x]*K2[x]
-        w[2] = w[2] + apha*e[x]*I[x]
+        w[2] = w[2] + apha*e[x]*I1[x]
     epoch += 1
     #y += 1
     return  w
 
 
 # e = w1k1 + w2k2 + w3i
-print(Learing(W, H, E, I, K1, K2, w))
+print(Learing(W, H, E, I1, K1, K2, w))
