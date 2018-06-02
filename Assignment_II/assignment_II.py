c3d2aaa9e56fd63747d52f2af9ec5c073cd2dedb
Macropodus <orangeandy12@gmail.com>
2018-06-02 21:55:22 +0800

    Assignment II_II

--- Assignment_II/assignment_II.py
+++ Assignment_II/assignment_II.py
@@ -1,3 +1,12 @@
 import numpy as np
-from PIL import Image
+# from sklearn import datasets
+from sklearn.datasets import fetch_mldata
+import matplotlib.pyplot as plt
+
+
+mnist = fetch_mldata('MNIST original')
+# digits = datasets.load_digits()
+# plt.figure(1, figsize=(3, 3))
+# plt.imshow(digits.images[-1], cmap=plt.cm.gray_r, interpolation='nearest')
+# plt.show()
 
