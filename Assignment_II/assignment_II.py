5331a4af39674e6184bef55d106dd09b31f193fb
Macropodus <orangeandy12@gmail.com>
2018-06-28 22:38:07 +0800

    Assignment II_V

--- Assignment_II/assignment_II.py
+++ Assignment_II/assignment_II.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
+import scipy
 
 # The digits dataset
 digits = datasets.load_digits()
